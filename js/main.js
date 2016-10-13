3055bc6f3aeafb3c5e704217b0373a653090ce9d
gabescarbrough <gabescarbrough@gmail.com>
2016-10-12 23:56:40 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$('.typed').typeIt({
+  speed: 100,
+  lifeLike: true,
+  autoStart: false,
+  loop: true,
+  loopDelay: 0
+})
+.tiType('Web Developer')
+.tiPause(3000)
+.tiDelete()
+.tiType('Designer')
+.tiPause(3000)
+.tiDelete()
+.tiType('Javascript Dreamer')
+.tiPause(3000)
+.tiDelete()
+.tiType('Passionate Rubyist')
+.tiPause(3000)
+.tiDelete()
+.tiType('Pizza Enthusiast')
+.tiPause(3000)
+.tiDelete()
